63c2601c4a7b4b5c15ebb6fbea8da89fa229226f
Jongsu Park <pjs7678@gmail.com>
2015-11-13 20:01:08 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='docx-replace',
+      packages = 'docx-replace',
+      version='0.1',
+      description='Replace words in docx file',
+      author='Jongsu Park',
+      author_email='pjs7678@gmail.com',
+      long_description=open('README.md').read(),
+      url='https://github.com/pjs7678/docx-replace',
+)
